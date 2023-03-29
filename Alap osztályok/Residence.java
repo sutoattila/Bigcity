9666d39892d9102d2d864508f455d916d3140885
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:24:47 +0000

    Upload New File

---
+++ Alap osztályok/Residence.java
@@ -0,0 +1,17 @@
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class Residence extends PrivateZone {
+    @Override
+    public int upgrade(){
+        //TODO
+        return 1;
+    }
+
+    public Residence() {
+    }
+    
+}
