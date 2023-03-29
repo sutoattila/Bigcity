a1d1c78031a414067c2b063901f12e28ca914b0f
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:26:20 +0000

    Upload New File

---
+++ Alap osztályok/Service.java
@@ -0,0 +1,13 @@
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class Service extends PrivateZone {
+    @Override
+    public int upgrade(){
+        //TODO
+        return 1;
+    }
+}
