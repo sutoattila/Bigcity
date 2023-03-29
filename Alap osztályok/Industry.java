4df6479862dbb42014cc20f6376afbdaced1cfc6
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:23:17 +0000

    Upload New File

---
+++ Alap osztályok/Industry.java
@@ -0,0 +1,13 @@
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class Industry extends PrivateZone {
+    @Override
+    public int upgrade(){
+        //TODO
+        return 1;
+    }
+}
