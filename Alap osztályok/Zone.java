8a9fa3cb20dcc4413628a70dc8e728ceb5ab4a56
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:27:11 +0000

    Upload New File

---
+++ Alap osztályok/Zone.java
@@ -0,0 +1,55 @@
+package bigcity;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public abstract class Zone {
+    protected int width;
+    protected int height;
+    protected int price;
+    protected int size;
+    protected int capacity;
+    protected int level;
+
+    protected ArrayList<Person> people;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+        
+    public int getPrice() {
+        return price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    
+    public boolean isConnectedToRoad(){
+        //TODO
+        return true;
+    }
+    public void destroy(){
+        //TODO
+    }
+    public abstract int upgrade();
+}
