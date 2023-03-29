152b2257e572670796d01e4824015892751bec9a
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:24:26 +0000

    Upload New File

---
+++ Alap osztályok/PublicZone.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public abstract class PublicZone extends Zone {
+    protected int maintenanceCost;
+
+    public int getMaintenanceCost() {
+        return maintenanceCost;
+    }
+    
+}
