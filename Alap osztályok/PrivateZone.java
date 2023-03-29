a88120ab28257ef4ca23a06fe768548a0baf03f0
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:24:06 +0000

    Upload New File

---
+++ Alap osztályok/PrivateZone.java
@@ -0,0 +1,12 @@
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public abstract class PrivateZone extends Zone {
+    public int collectTax(){
+        //TODO
+        return 0;
+    }
+}
