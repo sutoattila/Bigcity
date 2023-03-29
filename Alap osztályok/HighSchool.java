28e563b6b57ea9311737f2a0cb1cf5f6b6b1c00b
Sütő Attila <gbqx7g@inf.elte.hu>
2023-03-29 17:22:51 +0000

    Upload New File

---
+++ Alap osztályok/HighSchool.java
@@ -0,0 +1,23 @@
+package bigcity;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class HighSchool extends School {
+    
+    @Override
+    public int upgrade(){
+        //TODO
+        return 1;
+    }
+    
+    @Override
+    public void educate(Person person){
+        //TODO
+    }
+
+    public HighSchool() {
+    }
+    
+}
