14015f5cba39962ed5bd82f7606b35946cd9ea96
Mihalkó Norbert Ádám <mihalko.norbi@gmail.com>
2023-03-30 21:17:20 +0200

    BuildPanel merged with grid

---
+++ src/buildPanel/BuildPanel.java
@@ -0,0 +1,65 @@
+package buildPanel;
+
+import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.CursorSignal;
+import model.Engine;
+
+/**
+ * @author mihalkonorbi
+ */
+public class BuildPanel extends JPanel {
+    public static BuildButton selected = null;
+    
+    /**
+     * Sets the given button as selected (also changes it's color to yellow, 
+     * and sets the previous selected back to default color scheme)
+     * @param button
+     */
+    public static void selectButton(BuildButton button){
+        deleteSelection();
+        selected = button;
+        selected.setBackground(Color.yellow);
+        selected.btnPanel.setBackground(Color.yellow);
+    }
+    
+    /**
+     * Drops the selection from the selected button
+     */
+    public static void deleteSelection(){
+        if(selected != null){
+            selected.setBackground(Color.gray);
+            selected.btnPanel.setBackground(Color.gray);
+            selected = null;
+        }
+    }
+
+    /**
+     * Constructor
+     * @param builds The BuildButtons you want to display
+     */
+    public BuildPanel(BuildButton... builds){
+        super();
+        XButton exit = new XButton(new BuildButton.deleteSelected());
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JPanel exitPanel = new JPanel();
+        exitPanel.setLayout(new BoxLayout(exitPanel, BoxLayout.X_AXIS));
+        exitPanel.add(Box.createHorizontalGlue());
+        exitPanel.add(exit);
+        exitPanel.setAlignmentX(0);
+        exitPanel.setBackground(Color.GREEN);
+        add(exitPanel);
+
+        JPanel buildPanel = new JPanel();
+        buildPanel.setLayout(new BoxLayout(buildPanel, BoxLayout.Y_AXIS));
+        buildPanel.setBackground(Color.GREEN);
+        for (BuildButton buildButton : builds) {
+            buildPanel.add(buildButton);
+        }
+        add(buildPanel);
+    }
+}
--- src/view/BigCityJframe.java
+++ src/view/BigCityJframe.java
@@ -1,6 +1,5 @@
 package view;
 
-import model.Zone;
 import grid.Grid;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -16,6 +15,8 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import model.CursorSignal;
 import model.Engine;
 import res.Assets;
+import buildPanel.BuildPanel;
+import buildPanel.BuildButton;
 
 public class BigCityJframe extends JFrame {
 
@@ -50,119 +51,108 @@ public class BigCityJframe extends JFrame {
         super("BigCity");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        //*
+        
         int fieldSize = 100;
         int width = 5;
         int height = 5;
-         //*/
-        /*
-        int fieldSize = 20;
-        int width = 40;
-        int height = 30;
-        */
+        
         assets = new Assets();
 
         engine = new Engine(width, height);
 
         topPanel = new JPanel();
-        //The width calculated explicitly.
-        //The -1 indicates that we don't have control over it.
-        topPanel.setPreferredSize(
-                new Dimension(-1, 50));
+        topPanel.setPreferredSize(new Dimension(-1, 50));
         topPanel.setBackground(Color.LIGHT_GRAY);
-
         add(topPanel, BorderLayout.NORTH);
 
-        rightPanel = new JPanel();
-        //The height calculated explicitly.
-        //The -1 indicates that we don't have control over it.
-        rightPanel.setPreferredSize(
-                new Dimension(100, -1));
-        rightPanel.setBackground(Color.DARK_GRAY);
-
-        police = new JButton("Police 1x1");
-        police.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.POLICE);
-                engine.setImg(Assets.police);
-            }
-        });
-        rightPanel.add(police);
-        university = new JButton("University 2x2");
-        university.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.UNIVERSITY);
-                engine.setImg(Assets.university);
-            }
-        });
-        rightPanel.add(university);
-        service = new JButton("Service 1x1");
-        service.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.SERVICE);
-                engine.setImg(Assets.copperS);
-            }
-        });
-        rightPanel.add(service);
-        industry = new JButton("Industry 1x1");
-        industry.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.INDUSTRY);
-                engine.setImg(Assets.copperI);
-            }
-        });
-        rightPanel.add(industry);
-        road = new JButton("Road 1x1");
-        road.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.ROAD);
-                engine.setImg(Assets.roadNS);
-            }
-        });
-        rightPanel.add(road);
-        stadium = new JButton("Stadium 2x2");
-        stadium.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.STADIUM);
-                engine.setImg(Assets.stadium);
-            }
-        });
-        rightPanel.add(stadium);
-        highScool = new JButton("High School 1x2");
-        highScool.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.HIGH_SCHOOL);
-                engine.setImg(Assets.highSchool);
-            }
-        });
-        rightPanel.add(highScool);
-        residence = new JButton("Residence 1x1");
-        residence.addActionListener(new ActionListener() {
+        //TODO: nem volt kész a select gomb így azt ki kell javítani (XButton-ben)
+        
+        BuildButton lakohely = new BuildButton("buildPanel/images/house.png", "Lakóhely", 50);
+        BuildButton ipariTerulet = new BuildButton("buildPanel/images/factory.png", "Ipari terület", 50);
+        BuildButton szolgaltatas = new BuildButton("buildPanel/images/store.png", "Szolgáltatás", 50);
+        BuildButton ut = new BuildButton("buildPanel/images/road.png", "Út", 50);
+        BuildButton rendorseg = new BuildButton("buildPanel/images/police.png", "Rendőrség", 300);
+        BuildButton stadion = new BuildButton("buildPanel/images/stadium.png", "Stadion", 300);
+        BuildButton iskola = new BuildButton("buildPanel/images/school.png", "Iskola", 300);
+        BuildButton egyetem = new BuildButton("buildPanel/images/university.png", "Egyetem", 500);
+        
+        lakohely.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 engine.setCursorSignal(CursorSignal.RESIDENCE);
                 engine.setImg(Assets.copperR);
             }
         });
-        rightPanel.add(residence);
-
-        //TODO (Not complete.)
-        selectMode = new JButton("select mode");
-        selectMode.addActionListener(new ActionListener() {
+        
+        ipariTerulet.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                engine.setCursorSignal(CursorSignal.SELECT);
+                engine.setCursorSignal(CursorSignal.INDUSTRY);
+                engine.setImg(Assets.copperI);
             }
         });
-        rightPanel.add(selectMode);
+        
+        szolgaltatas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.SERVICE);
+                engine.setImg(Assets.copperS);
+            }
+        });
+        
+        ut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.ROAD);
+                engine.setImg(Assets.roadNS);
+            }
+        });
+        
+        rendorseg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.POLICE);
+                engine.setImg(Assets.police);
+            }
+        });
+        
+        stadion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.STADIUM);
+                engine.setImg(Assets.stadium);
+            }
+        });
+        
+        iskola.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.HIGH_SCHOOL);
+                engine.setImg(Assets.highSchool);
+            }
+        });
+        
+        egyetem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                engine.setCursorSignal(CursorSignal.UNIVERSITY);
+                engine.setImg(Assets.university);
+            }
+        });
 
+        BuildPanel buildPanel = new BuildPanel(
+            lakohely,
+            ipariTerulet,
+            szolgaltatas,
+            ut,
+            rendorseg,
+            stadion,
+            iskola,
+            egyetem
+            );
+        
+        
+        // IDEIGLENES GOMB -----------------------------------------------------
         destroyZone = new JButton("destroyZone");
         destroyZone.addActionListener(new ActionListener() {
             @Override
@@ -170,8 +160,8 @@ public class BigCityJframe extends JFrame {
                 engine.setCursorSignal(CursorSignal.DESTROY);
             }
         });
-        rightPanel.add(destroyZone);
-
+        buildPanel.add(destroyZone);
+        
         /*TODO: Finish select mode first! Can be used only if an upgradable zone
         //is selected. Appears on the new stat JPanel on the right if a zone is
         //selected.
@@ -182,19 +172,12 @@ public class BigCityJframe extends JFrame {
                 //TODO
             }
         });
-        rightPanel.add(upgrade);
          */
-        fillGrid = new JButton("fill grid");
-        fillGrid.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                grid.fillGrid();
-            }
-        });
-        rightPanel.add(fillGrid);
-
-        add(rightPanel, BorderLayout.EAST);
-
+        
+        // ---------------------------------------------------------------------
+        
+        add(buildPanel, BorderLayout.EAST);
+        
         grid = new Grid(fieldSize, width, height, engine);
         add(grid, BorderLayout.WEST);
 
