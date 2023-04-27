f5b640d473277542ec6a6fbda4e0b0511c3e7b3f
Sütő Attila <St Attila>
2023-04-27 12:43:25 +0200

    test

---
+++ test/model/EngineTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package model;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class EngineTest {
+    
+    public EngineTest() {
+    }
+    
+   
+    /**
+     * Test of setImg method, of class Engine.
+     */
+    @Test()
+    public void testSetImg() {
+        assertEquals(1, 1);
+    }
+
+
+
+    
+}
