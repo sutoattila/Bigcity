693c1e8c561168aee6996d15e26120e90256141a
zbuaz6_IM <zbuaz6@inf.elte.hu>
2023-04-29 18:22:06 +0200

    Couldn't load the txt files using the cmd terminal. The names are hard coded now.

--- src/model/CitizenGenerator.java
+++ src/model/CitizenGenerator.java
@@ -5,8 +5,10 @@ import bigcity.Person;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import res.ResourceLoader;
 
 public class CitizenGenerator {
 
@@ -14,14 +16,47 @@ public class CitizenGenerator {
     private List<String> femaleNames;
 
     public CitizenGenerator() {
-        try {
-            Path path = Path.of("./src/res/citizenNames/maleNames.txt");
+        /*try {
+            Path path = Path.of("res/citizenNames/maleNames.txt");
             this.maleNames = Files.lines(path).toList();
-            path = Path.of("./src/res/citizenNames/femaleNames.txt");
+            path = Path.of("res/citizenNames/femaleNames.txt");
             this.femaleNames = Files.lines(path).toList();
         } catch (IOException ex) {
             System.err.println("Couldn't open the file.");
-        }
+        }*/
+ /*
+        maleNames = ResourceLoader
+                .loadTxt("res/citizenNames/maleNames.txt")
+                .toList();
+        
+        
+        femaleNames = ResourceLoader
+                .loadTxt("res/citizenNames/femaleNames.txt")
+                .toList();
+         */
+        maleNames = new ArrayList<>();
+        maleNames.add("Olivia");
+        maleNames.add("Emma");
+        maleNames.add("Charlotte");
+        maleNames.add("Amelia");
+        maleNames.add("Ava");
+        maleNames.add("Sophia");
+        maleNames.add("Isabella");
+        maleNames.add("Mia");
+        maleNames.add("Evelyn");
+        maleNames.add("Harper");
+        
+        femaleNames=new ArrayList<>();
+        femaleNames.add("Liam");
+        femaleNames.add("Noah");
+        femaleNames.add("Oliver");
+        femaleNames.add("Elijah");
+        femaleNames.add("James");
+        femaleNames.add("William");
+        femaleNames.add("Benjamin");
+        femaleNames.add("Lucas");
+        femaleNames.add("Henry");
+        femaleNames.add("Theodore");
     }
 
     public Person createCitizen() {
