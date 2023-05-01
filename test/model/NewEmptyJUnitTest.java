f4e2e42ada0844765f2de3376390431dcfa0c0a6
Sütő Attila <St Attila>
2023-05-01 13:23:34 +0200

    test

---
+++ test/model/NewEmptyJUnitTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Sütő Attila
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        assertTrue(false);
+    }
+}
